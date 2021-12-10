260d2506975c80550d20f6c15c37ee18e37f0193
Enea Jahollari <jahollarienea14@gmail.com>
2021-12-10 10:25:44 +0100

    Change UserForRegisterDTO.cs

--- DTOs/User/UserForRegisterDTO.cs
+++ DTOs/User/UserForRegisterDTO.cs
@@ -20,11 +20,7 @@ namespace ToursApi.DTOs.User
         public string Password { get; set; } = null!;
 
         [Required] public string Name { get; set; } = null!;
-        [Required] public string Gender { get; set; } = null!;
-        [Required] public DateTime Birthday { get; set; }
-        [Required] public string KnownAs { get; set; } = null!;
-        [Required] public string City { get; set; } = null!;
-        [Required] public string Country { get; set; } = null!;
+        public string ImageUrl { get; set; } = null!;
 
         public DateTime CreatedAt { get; set; }
         public DateTime LastActive { get; set; }
